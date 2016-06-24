920d2dffcd0a19d02025d57af9b0689bce539e95
phlummox <phlummox2@gmail.com>
2016-06-25 00:38:32 +0930

    added doctest tests.

--- src/GHCi/History/Parse.hs
+++ src/GHCi/History/Parse.hs
@@ -25,6 +25,18 @@ import qualified Parsing.ReadP as P
 -- otherwise "Text.ParserCombinators.ReadP".
 --
 -- Returns either an error message, or a list of 'HistoryItem's.
+--
+-- Examples
+--
+-- >>> :{
+--  let myHistory = unlines [
+--          "-1  : fib (src/Stuff.hs:52:8-16)"
+--        , "<end of history>" ]
+-- :}
+--
+-- >>> parseHistory myHistory
+-- Right [HistoryItem {histStepNum = -1, funcName = "fib", fileName = "src/Stuff.hs", startPos = FilePos {lineNum = 52, colNum = 8}, endPos = FilePos {lineNum = 52, colNum = 16}}] 
+--
 parseHistory :: String -> Either String [GHCi.History.HistoryItem]
 parseHistory str =
   P.parse C.history "(interactive)" str
